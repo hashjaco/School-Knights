9ed5c171aec78429b8c578d936f13b4c21ca6a6f
Hashim Jacobs <hashjaco@github.com>
2019-08-06 02:37:14 -0700

    control/input field container, create input field index file containing destination and pickup textfield exports

--- school_knights/SK_USERS/App.js
+++ school_knights/SK_USERS/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapSection from './containers/MapSection'
+import ControlSection from './containers/ControlSection';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <MapSection />
+      <ControlSection />
     </View>
   );
 }
--- school_knights/SK_USERS/containers/ControlSection/index.js
+++ school_knights/SK_USERS/containers/ControlSection/index.js
@@ -1,14 +1,35 @@
-import React, { Component } from 'react'
-import { View } from 'react-native'
-import { Button, Container, InputGroup, Input, Spinner } from 'native-base'
+import React, { Component } from "react";
+import { View, StyleSheet } from "react-native";
+import { Button, Container, Spinner } from "native-base";
+import { Input } from "react-native-elements";
 
 export default class ControlSection extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
       location: null,
-      destination: null
-    }
+      destination: null,
+      isMatched: false,
+      isSearching: false
+    };
+  }
+
+  render() {
+    return <View style={styles.container}></View>;
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    display: 'flex',
+    height: '20%',
+    width: '100%',
+    backgroundColor: '#fff',
+    borderColor: '#000',
+    borderWidth: 1,
+    borderRadius: 10,
+    bottom: 0
+  }
+});

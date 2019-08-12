390746881fcca8fd2e59864ab710db9519ab2a3a
Hale Ramsey <Hale_ramsey@yahoo.com>
2019-08-12 00:37:26 -0700

    added in side menu bar that slides out with links to other pages

--- school_knights/SK_USERS/App.js
+++ school_knights/SK_USERS/App.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import MapSection from './containers/MapSection'
+import MapSection from './containers/MapSection';
 import ControlSection from './containers/ControlSection';
+import DrawerNavigator from './components/DrawerNavigator/DrawerNavigator';
 
-export default function App() {
+
+
+export default class App extends React.Component {
+  render() {
   return (
+    
     <View style={styles.container}>
-      <MapSection />
-      <ControlSection />
+      <DrawerNavigator/>
+     {/*} <MapSection />
+      <ControlSection /> */}
     </View>
+   
   );
 }
+}
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
